2a2171eec5d552c15c9b224c2db94e10139ccacc
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-18 20:02:27 +0530

    [vishal] Creating linked list Uc1

---
+++ Uc1_InsertLinkedList/InsertLinkedList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc1_InsertLinkedList
+{
+    class InsertLinkedList
+    {
+        public Node head;   //defining the head possition of link list
+        public void Add(int deta)  // create add method in this class
+        {
+            Node node = new Node(deta);    // defining and creating object of node class and push the data of node class
+            if (this.head == null)
+            {
+                this.head = node;          // is null then push the node deta that have add into the head
+            }
+            else
+            {
+                Node temp = head;             // creating temp veriable of node
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("inserted into the link list:{0}", node.data);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Display Nodes");
+            Node temp = this.head;               // initializethe temp veriable in the node class
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data + "");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ Uc1_InsertLinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Uc1_InsertLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to the Linked list opration");
+            InsertLinkedList list = new InsertLinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+        }
+    }
+}
