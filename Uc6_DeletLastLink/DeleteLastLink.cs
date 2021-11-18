8cd96d8bb489c14412914b9d4aa237906ac39d89
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-18 20:18:12 +0530

    [vishal] DeleteLast element linked list Uc6

---
+++ Uc6_DeletLastLink/DeleteLastLink.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc6_DeletLastLink
+{
+    class DeleteLastLink
+    {
+
+        internal Node head;
+        internal void InserLast(int deta)  //  Apending in last and create add method in this class
+        {
+            Node new_node2 = new Node(deta);    // defining and creating object of node class and push the data of node class
+            if (this.head == null)             //checking the head is null or not
+            {
+                this.head = new_node2;          // is null then push the node deta that have add into the head
+            }
+            else
+            {
+                Node temp = head;             // creating temp veriable of node
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = new_node2;
+            }
+            Console.WriteLine("\ninserted into the link list:{0}", new_node2.data);
+        }
+        public void Display()  //display the data in linked list
+        {
+            Console.WriteLine("Display Nodes");
+            Node temp = this.head;               // initializethe temp veriable in the node class
+            if (temp == null)                  // checking linklist empty or not
+            {
+                Console.Write("Linked list is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data + "");
+                    temp = temp.next;
+                }
+            }
+        }
+        //Deleted first node
+        public int DeleteFirstNode()
+        {
+            if (this.head == null)
+            {
+                return 0;
+
+            }
+            int deleteNode = this.head.data;
+            this.head = this.head.next;
+            return deleteNode;
+        }
+        // deleted last Node
+        public int DeleteLastNode()
+        {
+            Node newNod = this.head;
+            if (this.head == null)
+            {
+                return 0;
+            }
+            if (this.head.next == null)
+            {
+                this.head = null;
+                return 0;
+            }
+            while (newNod.next.next != null)
+            {
+                newNod = newNod.next;
+            }
+            int lastdeletedNode = newNod.next.data;
+            newNod.next = null;
+            return lastdeletedNode;
+        }
+    }
+}
---
+++ Uc6_DeletLastLink/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uc6_DeletLastLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n \nDelete Last Node  opration");
+            DeleteLastLink uc6list = new DeleteLastLink();
+            uc6list.InserLast(56);
+            uc6list.InserLast(30);
+            uc6list.InserLast(70);
+            uc6list.Display();
+            Console.WriteLine(" \nDeleted Last node valus is:" + uc6list.DeleteLastNode());
+            uc6list.Display();
+        }
+    }
+}
