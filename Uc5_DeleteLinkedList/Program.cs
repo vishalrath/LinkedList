54b13f80694f7e144feca61c48bca4f35a0ca841
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-18 20:15:22 +0530

    [vishal] delete element linked list Uc5

---
+++ Uc5_DeleteLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uc5_DeleteLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n \nDelete first Node  opration");
+            DeletLinkedList uc5list = new DeletLinkedList();
+            uc5list.InserLast(56);
+            uc5list.InserLast(30);
+            uc5list.InserLast(70);
+            uc5list.Display();
+            Console.WriteLine(" \nDeleted first node valus is:" + uc5list.DeleteFirstNode());
+            uc5list.DeleteFirstNode();
+        }
+    }
+}
