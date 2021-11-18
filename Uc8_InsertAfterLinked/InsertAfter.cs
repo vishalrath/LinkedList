4391c6af3a9c81e10c98635a05cdcbb08fcf2f96
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-18 20:21:48 +0530

    [vishal] InsertAfter element linked list Uc8

---
+++ Uc8_InsertAfterLinked/InsertAfter.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc8_InsertAfterLinked
+{
+    class InsertAfter
+    {
+        public Node head;   //defining the head possition of link list
+        public void Add(int deta)  // create add method in this class
+        {
+            Node node = new Node(deta);    // defining and creating object of node class and push the data of node class
+            if (this.head == null)
+            {
+                this.head = node;          // is null then push the node deta that have add into the head
+            }
+            else
+            {
+                Node temp = head;             // creating temp veriable of node
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("inserted into the link list:{0}", node.data);
+        }
+        public void AddAfter(int data) //insert between two node
+        {
+            Node previousnode = new Node(data);
+            previousnode = this.head;
+            if (previousnode.next.next == null)
+            {
+                Console.WriteLine(" previous node is null");
+                return;
+            }
+            Node newNode = new Node(data);          //creade a obj of Node newNode Address the memory And data
+            newNode.next = previousnode.next.next;  // previous node to next to is assign after next node
+            previousnode.next.next = newNode;      // previous.next.next is assign value of new node
+
+        }
+        public void Display()  //display the data in linked list
+        {
+            Console.WriteLine("Display Nodes");
+            Node temp = this.head;               // initializethe temp veriable in the node class
+            if (temp == null)                  // checking linklist empty or not
+            {
+                Console.Write("Linked list is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write(" " + temp.data + "");
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
---
+++ Uc8_InsertAfterLinked/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uc8_InsertAfterLinked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n \nAddAfter  Node  opration");
+            InsertAfter uc8add = new InsertAfter();
+            uc8add.Add(56);
+            uc8add.Add(30);
+            uc8add.Add(70);
+            uc8add.Display();
+            uc8add.AddAfter(40);
+            uc8add.Display();
+        }
+    }
+}
