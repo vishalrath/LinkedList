a0c48035c295008256a87bf739a45874fdd06848
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-18 20:19:46 +0530

    [vishal] Search element linked list Uc7

---
+++ Uc7_SearchLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uc7_SearchLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n \nSearch  Node  opration");
+            SearchLinked uc7list = new SearchLinked();
+            uc7list.InserLast(56);
+            uc7list.InserLast(30);
+            uc7list.InserLast(70);
+            uc7list.Display();
+            Console.WriteLine(" \nSearch  node valus is:" + uc7list.search(30));
+            uc7list.Display();
+        }
+    }
+}
