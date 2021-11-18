de0eb4e2c464046510d080e53592226744d6f08b
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-18 20:23:25 +0530

    [vishal] DeleteSize element linked list Uc9

---
+++ Uc9_DeleteSizeLinkedList/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc9_DeleteSizeLinkedList
+{
+    class Node
+    {
+        internal int data; //declare the integer type data variable
+        internal Node next; //next varible is Node type class under the considar as a integer type (next) variable
+        internal int possi;
+        internal Node(int data) //here is constructor
+        {
+            this.data = data; //value assgined to the instance variables
+            next = null;
+        }
+    }
+}
---
+++ Uc9_DeleteSizeLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uc9_DeleteSizeLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n \nSpecific delete  Node  opration");
+            DeleteSizeLinked uc9list = new DeleteSizeLinked();
+            uc9list.Add(56);
+            uc9list.Add(30);
+            uc9list.Add(70);
+            uc9list.AddAfter(40);
+            uc9list.Display();
+            uc9list.DeleteNode(40);
+            uc9list.Display();
+            Console.ReadLine();
+
+        }
+    }
+}
